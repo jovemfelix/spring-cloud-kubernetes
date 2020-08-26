059713f2a0085b5fc4286df57bda4d7f02167e33
Renato Alves Felix <rfelix@redhat.com>
2020-08-25 21:38:05 -0300

    usando versao 0.16

--- spring-cloud-kubernetes-examples/kubernetes-reload-example/src/main/java/io/fabric8/spring/cloud/kubernetes/examples/MyBean.java
+++ spring-cloud-kubernetes-examples/kubernetes-reload-example/src/main/java/io/fabric8/spring/cloud/kubernetes/examples/MyBean.java
@@ -16,19 +16,20 @@
  */
 package io.fabric8.spring.cloud.kubernetes.examples;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MyBean {
-
     @Autowired
     private MyConfig config;
 
     @Scheduled(fixedDelay = 5000)
     public void hello() {
-        System.out.println("The message is: " + config.getMessage());
+        log.info("The message is: " + config.getMessage());
     }
 
 
